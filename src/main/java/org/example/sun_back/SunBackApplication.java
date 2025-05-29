058025a67bae8ace03e593c48166ccb611c248c4
teodorposhyvak <poshyvak.teodor.t@gmail.com>
2025-05-29 21:13:11 +0300

    init commit

---
+++ src/main/java/org/example/sun_back/SunBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.sun_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/sun_back/SunBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sun_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
