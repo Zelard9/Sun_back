b965bda5e555ec5b62a61e46181a8b4d923c818a
Zelard9 <91178042+Zelard9@users.noreply.github.com>
2025-06-17 19:19:41 +0300

    Update SecurityConfig.java

--- src/main/java/org/example/sun_back/config/security/SecurityConfig.java
+++ src/main/java/org/example/sun_back/config/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("http://127.0.0.1:5500", "http://localhost:8080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
